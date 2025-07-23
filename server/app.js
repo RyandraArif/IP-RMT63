2c33de3d0c9a009ef97c553537e8a6eff8efd112
unknown <ryandraarif@gmail.com>
2025-07-23 23:09:46 +0700

    feat:animeController, refactor:app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const AnimeController = require("./controllers/animeController");
+const UserController = require("./controllers/userController");
+const FavoriteController = require("./controllers/favoriteController");
+const authentication = require("./middlewares/authentication");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+app.post("/register", UserController.register);
+app.post("/login", UserController.login);
+app.post("/login/google", UserController.googleLogin);
+
+app.use(authentication);
+app.patch("/users/genre", UserController.updateGenre);
+
+app.get("/animes", AnimeController.getAnime);
+app.get("/animes/recommendations", AnimeController.getRecommendation);
+app.get("/animes/:id", AnimeController.getAnimeById);
+
+app.post("/favorites", FavoriteController.addFavorite);
+app.get("/favorites", FavoriteController.getUserFavorites);
+app.delete("/favorites/:id", FavoriteController.deleteFavorite);
+
+module.exports = app;
---
+++ server/controllers/animeController.js
@@ -0,0 +1,92 @@
+const { Anime } = require("../models");
+const { Op } = require("sequelize");
+const qs = require("qs");
+
+module.exports = class AnimeController {
+  static async getAnime(req, res, next) {
+    try {
+      const { filter, sort, page, search } = qs.parse(req.query);
+
+      let queryOptions = {
+        where: {},
+        limit: 10,
+        offset: 0,
+      };
+
+      if (search) {
+        queryOptions.where.title = { [Op.iLike]: `%${search}%` };
+      }
+
+      if (filter?.genres) {
+        queryOptions.where.genre = { [Op.overlap]: filter.genres.split(",") };
+      }
+
+      if (sort) {
+        queryOptions.order = [[sort.by || "mean_rating", sort.order || "DESC"]];
+      }
+
+      if (page?.size) queryOptions.limit = Number(page.size);
+      if (page?.number) {
+        queryOptions.offset = (Number(page.number) - 1) * queryOptions.limit;
+      }
+
+      const { count, rows: animes } = await Anime.findAndCountAll(queryOptions);
+
+      res.status(200).json({
+        data: animes,
+        pagination: {
+          currentPage: Number(page?.number || 1),
+          dataPerPage: queryOptions.limit,
+          totalData: count,
+          totalPages: Math.ceil(count / queryOptions.limit),
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async getAnimeById(req, res, next) {
+    try {
+      const animeId = +req.params.id;
+      const animes = await Anime.findByPk(animeId);
+
+      if (!animes) {
+        // res.status(404).json({ message: `Data not found` })
+        // return
+        throw { name: "Not Found", message: `Data not found` };
+      }
+      res.status(200).json(animes);
+    } catch (err) {
+      // console.log(err, "<-------");
+      // res.status(404).json({ message: 'Data Not Found'})
+      next(err);
+    }
+  }
+
+  static async getRecommendation(req, res, next) {
+    try {
+      const user = req.user;
+
+      if (!user.genre) {
+        throw {
+          name: "BadRequest",
+          message: "Set your favorite genres first",
+        };
+      }
+
+      const recommendations = await Anime.findAll({
+        where: {
+          genre: {
+            [Op.overlap]: user.genre.split(","),
+          },
+        },
+        limit: 10,
+      });
+
+      res.json(recommendations);
+    } catch (err) {
+      next(err);
+    }
+  }
+};
